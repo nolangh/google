015764aeea0585b34d990371676fea8e18284fdf
nolangh <nolanghiggins@gmail.com>
2021-07-13 15:05:21 -0600

    added code for a different project. it will all be deleted

---
+++ index.js
@@ -0,0 +1,18 @@
+//return the highest and lowest number given.
+
+/*
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+*/
+
+function highAndLow(numbers) {
+    let newArray = numbers;
+        newArray.split(" ")
+    for(i = 0; i < newArray.length; i)
+    
+
+}
+
+const test = highAndLow("1 9 3 4 -5");
+console.log(test);
