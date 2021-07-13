9b9e152f3c525665a843a88ccefac894c55ef884
nolangh <nolanghiggins@gmail.com>
2021-07-13 17:41:35 -0600

    added more code for a different kata

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-//return the highest and lowest number given.
-
 /*
-highAndLow("1 2 3 4 5");  // return "5 1"
-highAndLow("1 2 -3 4 5"); // return "5 -3"
-highAndLow("1 9 3 4 -5"); // return "9 -5"
+The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
+
+Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+
+ANCHOR Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue?
+
+Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment. Otherwise return NO.
+
+Examples:
+tickets([25, 25, 50]) // => YES 
+tickets([25, 100]) // => NO. Vasya will not have enough money to give change to 100 dollars
+tickets([25, 25, 50, 50, 100]) // => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
 */
 
-function highAndLow(numbers) {
-    let newArray = numbers;
-        newArray.split(" ")
-    for(i = 0; i < newArray.length; i)
-    
-
-}
-
-const test = highAndLow("1 9 3 4 -5");
-console.log(test);
+function tickets(peopleInLine) {}
