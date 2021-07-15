0792f985e4d5be37d10c365b5a03d0a09c421a51
nolangh <nolanghiggins@gmail.com>
2021-07-15 12:03:23 -0600

    new kata

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 /*
-The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
 
-Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
 
-ANCHOR Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue?
+ANCHOR After your function finds the needle it should return a message (as a string) that says:
 
-Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment. Otherwise return NO.
+"found the needle at position " plus the index it found the needle, so:
 
-Examples:
-tickets([25, 25, 50]) // => YES 
-tickets([25, 100]) // => NO. Vasya will not have enough money to give change to 100 dollars
-tickets([25, 25, 50, 50, 100]) // => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
+findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+
+ANCHOR should return "found the needle at position 5"
+
 */
 
-// NOTE Vasya starts at $0
-// NOTE ticket is $25
-// NOTE Must be able to give change back
-
-function tickets(peopleInLine) {}
-function calc(input) {
-  let counter = i;
-
-  return;
+function findNeedle(haystack) {
 }
